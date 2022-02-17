cc10bf6e789118ac5f5ed01db41008abca2f2109
Laith Nurie <laithnurie@gmail.com>
2022-02-17 21:35:19 +0000

    vertical movement corrected

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -48,19 +48,26 @@ public class PlayerController : MonoBehaviour
         Move(_playerMovement);
     }
     
-    
     private void Move(Vector2 playerMovement)
     {
         if (playerMovement != Vector2.zero)
         {
-            // check negative or positive to switch the character around
-            gameObject.transform.localScale = new Vector3(playerMovement.x > 0 ? 1f : -1f, 1f, 1f);
+            gameObject.transform.localScale = new Vector3(GetDirection(playerMovement.x), 1f, 1f);
         }
 
-        _rigidbody2D.velocity = new Vector3(playerMovement.x * speed, playerMovement.y * speed);
-        
-        _characterController.ChangeAnimation(_rigidbody2D.velocity.x == 0
+        var newVelocity = new Vector3(playerMovement.x * speed, playerMovement.y * speed);
+        _rigidbody2D.velocity = newVelocity;
+
+        _characterController.ChangeAnimation(newVelocity.x == 0 && newVelocity.y == 0
             ? _characterController.Idle
             : _characterController.Walk);
+    }
+
+    private float GetDirection(float xVelocity)
+    {
+        // check negative or positive to switch the character around
+        if (xVelocity == 0) return gameObject.transform.localScale.x;
+        if (xVelocity < 0) return -1f;
+        return 1f;
     }
 }
